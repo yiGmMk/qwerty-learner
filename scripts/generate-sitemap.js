fb189f9c5d40be74649b4c94a3ea4ebe64217046
yi_t <marvelousme@163.com>
2025-10-14 18:02:31 +0800

    sitemap

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const routes = [
+  '',
+  'gallery',
+  'analysis',
+  'error-book',
+  'mobile',
+];
+
+const domain = 'https://typing.programnotes.cn';
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes.map(route => `
+  <url>
+    <loc>${domain}/${route}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+  </url>`).join('')}
+</urlset>
+`;
+
+const publicPath = path.resolve(__dirname, '../public');
+
+fs.writeFileSync(path.join(publicPath, 'sitemap.xml'), sitemap);
+
+console.log('sitemap.xml generated!');
